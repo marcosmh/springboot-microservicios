e19b5c35aeaec1540c81c0bcb5073b1a3bfc6e39
Marcos Magaña Hernández <imarcosmaganah@gmail.com>
2023-04-20 15:42:56 -0600

    Resilience4J- configuracion inyecccion de dependencias y por yml y properties

--- springboot-servicio-item/src/main/java/com/markcode/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/markcode/springboot/app/item/controllers/ItemController.java
@@ -2,8 +2,13 @@ package com.markcode.springboot.app.item.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -13,11 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 import com.markcode.springboot.app.item.models.Item;
 import com.markcode.springboot.app.item.models.Producto;
 import com.markcode.springboot.app.item.models.service.ItemService;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+
+
+
 
 @RestController
 public class ItemController {
+	
+	private final Logger logger = LoggerFactory.getLogger(ItemController.class);
 
+	@Autowired
+	private CircuitBreakerFactory cbFactory;
+	
 	@Autowired
 	@Qualifier("serviceFeign")
 	//@Qualifier("serviceRestTemplate")
@@ -26,19 +39,24 @@ public class ItemController {
 	@GetMapping("/listar")
 	public List<Item> listar(@RequestParam(name="nombre", required=false) 
 		String nombre, @RequestHeader(name="token-request", required=false) String token) {
-		System.out.println("nombre= " +  nombre +"  token= "+ token);
+		logger.info("nombre= " +  nombre +"  token= "+ token);
 		return itemService.findAll();
 	}
 	
-	@HystrixCommand(fallbackMethod = "metodoAlternativo")
+	//@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
-		return itemService.findById(id, cantidad);
+		//return itemService.findById(id, cantidad);
+		
+		return cbFactory.create("items")
+				.run( () -> itemService.findById(id, cantidad), e-> metodoAlternativo(id, cantidad, e) );
 	}
 	
 	
-	public Item metodoAlternativo(Long id, Integer cantidad) {
-		System.out.println("Metodo Alternativo...");
+	public Item metodoAlternativo(Long id, Integer cantidad, Throwable e) {
+		logger.info(e.getMessage());
+		
+		logger.info("Metodo Alternativo...");
 		Item item = new Item();
 		Producto producto = new Producto();
 		item.setCantidad(cantidad);
--- springboot-servicio-productos/src/main/java/com/markcode/springboot/app/productos/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/markcode/springboot/app/productos/controllers/ProductoController.java
@@ -1,8 +1,11 @@
 package com.markcode.springboot.app.productos.controllers;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -13,8 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.markcode.springboot.app.productos.models.entity.Producto;
 import com.markcode.springboot.app.productos.models.service.IProducto;
 
+
+
 @RestController
 public class ProductoController {
+	
+	private final Logger logger = LoggerFactory.getLogger(ProductoController.class);
 	
 	@Autowired
 	Environment env;
@@ -32,24 +39,28 @@ public class ProductoController {
 		return productoService.findAll()
 				.stream()
 				.map(producto -> {
-					//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-					producto.setPort(port);
+					producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+					//producto.setPort(port);
 					return producto;
 				}).collect(Collectors.toList());
 	}
 	
 	@GetMapping("/ver/{id}")
-	public Producto detalle(@PathVariable Long id) {
-		Producto producto = productoService.findById(id);
-		//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-		producto.setPort(port);
+	public Producto detalle(@PathVariable Long id) throws InterruptedException {
 		
-		/*
-		try {
-			Thread.sleep(2000L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}*/
+		if(id.equals(10L)) {
+			throw new IllegalStateException("Producto no encontrado");
+		}
+		
+		if(id.equals(7L)) {
+			TimeUnit.SECONDS.sleep(6L);
+		}
+		
+		
+		Producto producto = productoService.findById(id);
+		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		//producto.setPort(port);
+		logger.info("port: " + port);
 		
 		return producto;
 	}
