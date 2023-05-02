43f9156027204ad4a93fab3f996bfcaa8d990c86
Marcos Magaña Hernández <imarcosmaganah@gmail.com>
2023-05-02 11:48:23 -0600

    Spring Security Config

---
+++ springboot-servicio-oauth/src/main/java/springbootservicio/app/oauth/clients/UsuarioFeignClient.java
@@ -0,0 +1,16 @@
+package springbootservicio.app.oauth.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import springbootservicio.app.commons.usuarios.models.entity.Usuario;
+
+@FeignClient(name="servicio-usuarios")
+public interface UsuarioFeignClient {
+
+	@GetMapping("/usuarios/search/buscar-username")
+	Usuario findByUsername(@RequestParam String username);
+	
+	
+}
---
+++ springboot-servicio-oauth/src/main/java/springbootservicio/app/oauth/services/UsuarioService.java
@@ -0,0 +1,49 @@
+package springbootservicio.app.oauth.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import springbootservicio.app.commons.usuarios.models.entity.Usuario;
+import springbootservicio.app.oauth.clients.UsuarioFeignClient;
+
+@Service
+public class UsuarioService implements UserDetailsService {
+	
+	private final Logger logger = LoggerFactory.getLogger(UserDetailsService.class);
+	
+	@Autowired
+	private UsuarioFeignClient client;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario usuario = client.findByUsername(username);
+		
+		if(usuario == null) {
+			logger.info("username: ${username}");
+			throw new UsernameNotFoundException("Error en el login, no existe el usuario "+username+" en el sistema");
+		}
+		
+		List<GrantedAuthority> authorities = usuario.getRoles()
+				.stream()
+				.map(role -> new SimpleGrantedAuthority(role.getNombre()))
+				.peek(authority -> logger.info("Role: " + authority.getAuthority()))
+				.collect(Collectors.toList());
+		
+		logger.info("Usuario autenticado: " +  usuario.getUserName());
+		
+		return new User(usuario.getUserName(), usuario.getPassword(), usuario.getEnabled(), 
+				true, true, true, authorities);
+	}
+
+}
