77be5b7680eb7126fc31285ad08c8646d9bf1c01
Marcos Magaña Hernández <imarcosmaganah@gmail.com>
2023-05-03 12:21:11 -0600

    se agrego configuracion AutorizationServerConfig

--- springboot-servicio-usuarios/src/main/resources/import.sql
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,7 +1,6 @@
-INSERT INTO usuarios (user_name, password, enabled, nombre, apellido, email) VALUES ('markcode','666999',1,'Marcos','Magana','markcode@code.com');
-
-
+INSERT INTO usuarios (user_name, password, enabled, nombre, apellido, email) VALUES ('markcode','$2a$10$SczeowgV1.2KSFQKcvMhHeOH.ND2iZwKDpriTFAu67fFVW4avDynq',1,'Marcos','Magana','markcode@code.com');
 INSERT INTO usuarios (user_name, password, enabled, nombre, apellido, email) VALUES ('admin','12346',1,'Administrador','Sistema','sistema@code.com');
+
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER');
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
