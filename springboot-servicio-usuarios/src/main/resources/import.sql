29ebd13d3b1b0b77f2d1d07fe9377b2661600d23
Marcos Magaña Hernández <imarcosmaganah@gmail.com>
2023-05-12 13:14:21 -0600

    configurando postgresql a servicio-usuarios

--- springboot-servicio-usuarios/src/main/resources/import.sql
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios (user_name, password, enabled, nombre, apellido, email) VALUES ('markcode','$2a$10$SczeowgV1.2KSFQKcvMhHeOH.ND2iZwKDpriTFAu67fFVW4avDynq',1,'Marcos','Magana','markcode@code.com');
-INSERT INTO usuarios (user_name, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$ZQ38LB1agPo9cKkYsFlR2.SpPd3WKPVMHvNE5pzHYp4fnRdsj5rsq',1,'Administrador','Sistema','sistema@code.com');
+INSERT INTO usuarios (user_name, password, enabled, nombre, apellido, email) VALUES ('markcode','$2a$10$SczeowgV1.2KSFQKcvMhHeOH.ND2iZwKDpriTFAu67fFVW4avDynq',true,'Marcos','Magana','markcode@code.com');
+INSERT INTO usuarios (user_name, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$ZQ38LB1agPo9cKkYsFlR2.SpPd3WKPVMHvNE5pzHYp4fnRdsj5rsq',true,'Administrador','Sistema','sistema@code.com');
 
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER');
